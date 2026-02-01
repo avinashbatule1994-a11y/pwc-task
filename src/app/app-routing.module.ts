4396982174f6a16b24c60c5078927691187159d5
Avinash Batule <avinash.batule1994@gmail.com>
2026-02-01 13:10:56 +0530

    created basic modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'workflows',
+    loadChildren: () =>
+      import('./features/workflow/workflow.module').then(m => m.WorkflowModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
